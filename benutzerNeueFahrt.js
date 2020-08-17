793a8e4dc1d88c2eb53cccb828d309c00bb7567e
jbage <jojobagert@web.de>
2020-08-17 14:44:05 +0200

    htmls und js beispiel

---
+++ benutzerNeueFahrt.js
@@ -0,0 +1,19 @@
+//initialize leaflet with links for different basemaps
+var mapLink = '<a href="http://www.esri.com/">Esri</a>';
+
+var satelliteMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: '&copy; ' + mapLink,
+  maxZoom: 20,
+});
+var topoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: ''
+});
+
+var worldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: ''
+});
+
+var benutzerFahrtMap = L.map('mapNeueFahrt', {
+  layers: [topoMap]
+})
+benutzerFahrtMap.locate({ setView: true, maxZoom: 20 });
