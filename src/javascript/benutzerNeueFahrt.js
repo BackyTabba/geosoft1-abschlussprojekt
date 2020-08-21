af47095364e502e938f925eab03419ae898063f0
jbage <jojobagert@web.de>
2020-08-21 09:42:40 +0200

    Ordnerstruktur

---
+++ src/javascript/benutzerNeueFahrt.js
@@ -0,0 +1,28 @@
+//initialize leaflet with links for different basemaps
+var mapLink = '<a href="http://www.esri.com/">Esri</a>';
+
+var satelliteMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: '&copy; ' + mapLink,
+  maxZoom: 20,
+});
+var topoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: ''
+});
+
+var worldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: ''
+});
+
+var baseMaps = {
+  "Satellite": satelliteMap,
+  "Topographic": topoMap,
+  "WSM": worldStreetMap
+};
+
+var benutzerFahrtMap = L.map('mapNeueFahrt', {
+  layers: [satelliteMap]
+});
+
+L.control.layers(baseMaps).addTo(benutzerFahrtMap);
+
+benutzerFahrtMap.locate({ setView: true, maxZoom: 20 });
